8c6ec3481ef525f5a52e0391767713511bc82756
HEMANTH_AINDYALA <hemanthandlayla@gmail.com>
2026-08-21 15:42:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
+
+
 from flask import Flask
 app=Flask(__name__)
 @app.route("/")
 def home():
-	return "flask application running successfully"
+	#flask application running successfully
+	return '''
+	<html>
+	    <body>
+		 <h1>Hello RGUKT</h1>
+	   </body>
+	</html>
+	'''
 if __name__==('__main__'):
     app.run(debug=True)
     app.run('0.0.0.0',5000)
